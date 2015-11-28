540601217200e6eab6efb8d96a87fdf3ccdb1be1
Christoph Zwirello <c.zwirello@thenewmotion.com>
2015-11-28 13:50:02 +0100

    make library client supply versionsUrl

--- endpoints/src/main/scala/com/thenewmotion/ocpi/handshake/HandshakeRoute.scala
+++ endpoints/src/main/scala/com/thenewmotion/ocpi/handshake/HandshakeRoute.scala
@@ -6,15 +6,13 @@ import org.joda.time.DateTime
 import scala.concurrent.ExecutionContext
 import scalaz._
 
-class HandshakeRoute(service: HandshakeService, currentTime: => DateTime = DateTime.now) extends JsonApi {
+class HandshakeRoute(service: HandshakeService, versionsUrl: String, currentTime: => DateTime = DateTime.now) extends JsonApi {
 
   def route(version: Version, auth: AuthToken)(implicit ec: ExecutionContext) = {
     import com.thenewmotion.ocpi.msgs.v2_0.OcpiJsonProtocol._
     import com.thenewmotion.ocpi.msgs.v2_0.Credentials._
 
     (post & extract(_.request.uri)) { credentialsUrl =>
-      //FIXME: extract baseUrl properly here:
-      val versionsUrl = spray.http.Uri(credentialsUrl.toString().replaceAll("""/\d+.\d+/credentials""", ""))
       entity(as[Creds]) { clientCreds =>
         onSuccess(service.startHandshake(version, auth, clientCreds, versionsUrl)) {
           case -\/(_) => reject()
