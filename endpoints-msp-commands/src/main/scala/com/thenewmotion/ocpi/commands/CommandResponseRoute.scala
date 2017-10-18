fc8450f04f47c8a5aeed193fb230d41794fc0570
Dan Brooke <d.brooke@newmotion.com>
2017-10-18 16:39:44 +0200

    Allow user of CommandResponseRoute to return response

--- endpoints-msp-commands/src/main/scala/com/thenewmotion/ocpi/commands/CommandResponseRoute.scala
+++ endpoints-msp-commands/src/main/scala/com/thenewmotion/ocpi/commands/CommandResponseRoute.scala
@@ -4,29 +4,30 @@ package commands
 import java.util.UUID
 
 import akka.http.scaladsl.server.Route
-import msgs.OcpiStatusCode.GenericSuccess
 import msgs.v2_1.Commands.{CommandResponse, CommandResponseType}
 import common.{EitherUnmarshalling, OcpiDirectives, OcpiRejectionHandler}
 import msgs._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 class CommandResponseRoute(
-  callback: (GlobalPartyId, UUID, CommandResponseType) => Future[Unit]
+  callback: (GlobalPartyId, UUID, CommandResponseType) => Future[Option[SuccessResp[Unit]]]
 ) extends JsonApi with EitherUnmarshalling with OcpiDirectives {
 
   import msgs.v2_1.DefaultJsonProtocol._
   import msgs.v2_1.CommandsJsonProtocol._
 
-  def route(apiUser: GlobalPartyId)(implicit executionContext: ExecutionContext): Route =
+  def route(apiUser: GlobalPartyId): Route =
     handleRejections(OcpiRejectionHandler.Default)(routeWithoutRh(apiUser))
 
-  private[commands] def routeWithoutRh(apiUser: GlobalPartyId)(implicit executionContext: ExecutionContext) =
+  private[commands] def routeWithoutRh(apiUser: GlobalPartyId) =
     (pathPrefix(JavaUUID) & pathEndOrSingleSlash) { commandId =>
       post {
         entity(as[CommandResponse]) { response =>
-          complete {
-            callback(apiUser, commandId, response.result).map(_ => SuccessResp(GenericSuccess))
+          rejectEmptyResponse {
+            complete {
+              callback(apiUser, commandId, response.result)
+            }
           }
         }
       }
