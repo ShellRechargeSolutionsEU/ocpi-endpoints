3e83e1e10049c5bbbae7be0afaadc6d3655ad43a
Dan Brooke <d.brooke@newmotion.com>
2017-11-09 11:30:46 +0100

    Fix newly merged TokensClient

--- endpoints-cpo-tokens/src/main/scala/com/thenewmotion/ocpi/tokens/TokensClient.scala
+++ endpoints-cpo-tokens/src/main/scala/com/thenewmotion/ocpi/tokens/TokensClient.scala
@@ -3,23 +3,26 @@ package tokens
 
 import akka.http.scaladsl.HttpExt
 import akka.http.scaladsl.model.Uri
-import akka.stream.ActorMaterializer
-import com.github.nscala_time.time.Imports._
-import common.OcpiClient
-import msgs.ErrorResp
+import akka.stream.Materializer
+import com.thenewmotion.ocpi.msgs.Ownership.Ours
+import common.{ErrRespUnMar, OcpiClient, PagedRespUnMar}
+import java.time.ZonedDateTime
+import msgs.{AuthToken, ErrorResp}
 import msgs.v2_1.Tokens.Token
-
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
-import scalaz._
 
-class TokensClient(implicit http: HttpExt) extends OcpiClient {
+class TokensClient(
+  implicit http: HttpExt,
+  errorU: ErrRespUnMar,
+  sucU: PagedRespUnMar[Token]
+) extends OcpiClient {
 
-  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-  import msgs.v2_1.OcpiJsonProtocol._
-
-  def getTokens(uri: Uri, auth: String, dateFrom: Option[DateTime] = None, dateTo: Option[DateTime] = None)
-    (implicit ec: ExecutionContext, mat: ActorMaterializer):
-  Future[ErrorResp \/ Iterable[Token]] =
+  def getTokens(
+    uri: Uri,
+    auth: AuthToken[Ours],
+    dateFrom: Option[ZonedDateTime] = None,
+    dateTo: Option[ZonedDateTime] = None
+  )(implicit ec: ExecutionContext, mat: Materializer): Future[Either[ErrorResp, Iterable[Token]]] =
     traversePaginatedResource[Token](uri, auth, dateFrom, dateTo)
 }
