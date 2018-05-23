92e6ca67fa00720625e4997b4492cc11b305b789
Christoph Zwirello <c.zwirello@newmotion.com>
2018-05-23 16:19:26 +0200

    bump sbt, build-seed and circe

--- msgs-circe/src/test/scala/com/thenewmotion/ocpi/msgs/circe/v2_1/CirceJsonSpec.scala
+++ msgs-circe/src/test/scala/com/thenewmotion/ocpi/msgs/circe/v2_1/CirceJsonSpec.scala
@@ -21,7 +21,7 @@ trait CirceJsonSpec extends GenericJsonSpec[Json, Decoder, Encoder] {
 
   override def objToJson[T : Encoder](t: T): Json = {
     // we can only remove the null keys when printing, so we print then re-parse, bit hacky but works
-    val printer = Printer.noSpaces.copy(dropNullKeys = true)
+    val printer = Printer.noSpaces.copy(dropNullValues = true)
     parse(printer.pretty(t.asJson))
   }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,3 @@
 resolvers += "TNM public" at "http://nexus.thenewmotion.com/content/groups/public"
 
-// https://github.com/sbt/sbt-release
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.6")
-
-addSbtPlugin("com.newmotion" % "sbt-build-seed" % "4.0.1" )
+addSbtPlugin("com.newmotion" % "sbt-build-seed" % "4.0.8" )
