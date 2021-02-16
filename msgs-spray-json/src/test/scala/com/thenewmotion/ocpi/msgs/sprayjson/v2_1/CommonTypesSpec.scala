793dc530cfd5c3155143dfa5f4937fcf682c5c65
Christoph Zwirello <c.zwirello@newmotion.com>
2021-02-16 15:09:47 +0100

    fix tiny 2.13 incompatibility

--- msgs-spray-json/src/test/scala/com/thenewmotion/ocpi/msgs/sprayjson/v2_1/CommonTypesSpec.scala
+++ msgs-spray-json/src/test/scala/com/thenewmotion/ocpi/msgs/sprayjson/v2_1/CommonTypesSpec.scala
@@ -1,6 +1,7 @@
 package com.thenewmotion.ocpi.msgs.sprayjson.v2_1
 
 import com.thenewmotion.ocpi.msgs.v2_1.GenericCommonTypesSpec
+import com.thenewmotion.ocpi.msgs.sprayjson.v2_1.DefaultJsonProtocol
 import spray.json._
 
 class CommonTypesSpec extends GenericCommonTypesSpec[JsValue, JsonReader, JsonWriter] with SprayJsonSpec {
